fd9cdddc00900f6d3e9beb7d7a052396dd251af1
Kawechai <lnwnice@gmail.com>
2025-05-22 01:01:47 +0700

    test

--- test_api_calls.py
+++ test_api_calls.py
@@ -6,19 +6,22 @@ def documented_login():
     url = "http://localhost/login.php"
     data = {"username": "admin", "password": "password"}
     response = requests.post(url, data=data)
+    response.raise_for_status()
     print("Documented login response:", response.text)
 
 # Undocumented API usage (not in Swagger)
 def undocumented_access():
     url = "http://localhost/undocumented.php"
-    data = {"value": "testing undocumented"}
     response = requests.post(url, data=data)
+    response.raise_for_status()
+    print("Undocumented response:", response.text)
     print("Undocumented response:", response.text)
     
 def documented_login():
     url = "https://reqres.in/#/login.php"
     data = {"username": "admin", "password": "password"}
     response = requests.post(url, data=data)
+    response.raise_for_status()
     print("Documented login response:", response.text)
 
 if __name__ == "__main__":
