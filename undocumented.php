4d6816c16555402f57a27503b4b48841ae7e6a99
kawechai <71128478+nicekak1@users.noreply.github.com>
2025-05-21 22:24:10 +0700

    Update undocumented.php

--- undocumented.php
+++ undocumented.php
@@ -1,6 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = $_POST['example'];
-    echo "Received: $data";
+    header('Content-Type: application/json');
+    $data = $_POST['value'] ?? 'empty';
+    echo json_encode(["message" => "Undocumented endpoint called", "value" => $data]);
+    http_response_code(200);
 }
 ?>
